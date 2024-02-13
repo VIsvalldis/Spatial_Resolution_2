f8adc0c31d22b66d60df9d3c042622a8febb1cec
VBurdin <vsevolod.tesl@gmail.com>
2024-02-13 19:24:23 +0700

    New Spatial Resolution with 21 strips

---
+++ build/CMakeFiles/Spatial_Resolution.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/Spatial_Resolution.cc" "CMakeFiles/Spatial_Resolution.dir/Spatial_Resolution.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/Spatial_Resolution.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/action.cc" "CMakeFiles/Spatial_Resolution.dir/action.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/action.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/construction.cc" "CMakeFiles/Spatial_Resolution.dir/construction.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/construction.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/detector.cc" "CMakeFiles/Spatial_Resolution.dir/detector.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/detector.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/event.cc" "CMakeFiles/Spatial_Resolution.dir/event.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/event.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/generator.cc" "CMakeFiles/Spatial_Resolution.dir/generator.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/generator.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/physics.cc" "CMakeFiles/Spatial_Resolution.dir/physics.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/physics.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/run.cc" "CMakeFiles/Spatial_Resolution.dir/run.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/run.cc.o.d"
+  "/home/vsevolod/GitHub/Spatial_Resolution_21_2/stepping.cc" "CMakeFiles/Spatial_Resolution.dir/stepping.cc.o" "gcc" "CMakeFiles/Spatial_Resolution.dir/stepping.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
